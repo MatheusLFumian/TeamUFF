5b2e75b6c2b7e9a343e8cf9317e661802c256d1f
Matheus Lessa Fumian <matheusfumian@gmail.com>
2021-03-03 22:22:08 -0300

    Initial API configuration

---
+++ database/migrations/2021_03_03_202620_create_parts_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePartsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('parts', function (Blueprint $table) {
+            $table->id();
+            $table->integer('company_id')->unsigned();
+            $table->foreign('company_id')->references('id')->on('companies')->onDelete('cascade');
+            $table->integer('seller_id')->unsigned();
+            $table->foreign('seller_id')->references('id')->on('seller');
+            $table->string('descricao');
+            $table->string('tipo');
+            $table->float('peso');
+            $table->float('volume');
+            $table->string('status');
+            $table->float('valor_frete');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('parts');
+    }
+}
